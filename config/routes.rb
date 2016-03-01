f08f85b04cb0e015ba4b4ded4e3112a0ad4ec537
Adam Hundley <adamhundley@gmail.com>
2016-03-01 15:35:17 -0700

    Viewing Cart Functionality
    Add dynamic cart item counter. Add cart total and individual item total. Add cart index view. Styling still needed for index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :products, only: [:index, :show]
-  resources :cart, only: [:create]
+  resources :cart, only: [:create, :index]
 
   get "/:name", to: "categories#show"
 
