eafa6da54652c38b0e17787dd1a9c609826b039e
Nick Weiland <nickweiland@gmail.com>
2016-02-29 17:42:07 -0700

    2 User views all products
    Add feature test for user to view all products. 2 User views all products
    
    Add feature test for user to view all products.

---
+++ spec/features/user_views_all_products_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "UserViewsAllProducts", type: :feature do
+  scenario "user views all products" do
+    category = Category.create(name:"coffee")
+    product = category.products.create(name:"Ethiopian", price:1500, description:"Ethiopian coffee is super good", image_url:"http://www.ethiopia-xperience.com/images/Pics_uploaded_by_Jos/EthiopianCoffee2010_586.jpg")
+
+    visit root_path
+
+    click_on "Shop all"
+
+    within "div#products" do
+      expect(page).to have_content(product.name)
+      expect(page).to have_link(product_path(product.id))
+    end
+  end
+end
