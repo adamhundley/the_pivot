b500625b22293ac80c33ba968655dff847eedfe1
David Whitaker <damwhit@gmail.com>
2016-03-02 14:13:39 -0700

    27 Existing user can log in and log out
    existing user is now able to log in and out of an account

---
+++ spec/features/user_can_login_to_existing_accounts_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.feature "UserCanLoginToExistingAccount", type: :feature do
+  scenario "user logs in to account" do
+    category = Category.create(name:"coffee")
+    product = category.products.create(name:"Ethiopian", price:1500, description:"Ethiopian coffee is super good", image_url:"http://www.ethiopia-xperience.com/images/Pics_uploaded_by_Jos/EthiopianCoffee2010_586.jpg")
+    user = User.create(first_name: "John", last_name: "Adams", email: "email@example.com", password: "password")
+
+    visit "/products/#{product.id}"
+    click_on "Add to cart"
+
+    visit "/"
+
+    click_on "login"
+
+    fill_in "email", with: user.email
+    fill_in "password", with: user.password
+
+    click_on "login"
+
+    expect(page).to have_content("Hey John, welcome to Little Owl.")
+
+    expect(current_path).to eq("/")
+
+    #within whatever
+    expect(page).to_not have_link("login")
+    expect(page).to have_content("John")
+    expect(page).to have_link("logout")
+    expect(page).to have_link("order history")
+    expect(page).to have_link("settings")
+
+    click_on "cart"
+    expect(page).to have_content("Ethiopian")
+
+    click_on "Continue Shopping"
+
+    expect(current_path).to eq("/products")
+
+    click_on "logout"
+
+    expect(page).to have_link("login")
+    expect(page).to_not have_link("logout")
+    expect(page).to_not have_link("order history")
+    expect(page).to_not have_link("settings")
+    expect(page).to_not have_content("John Adams")
+
+  end
+end
