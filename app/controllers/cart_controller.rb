43c9a9ad5985cc0f1458d02f6f2cf2f7da6bbba2
David Whitaker <damwhit@gmail.com>
2016-03-01 17:23:18 -0700

    user is now able to remove items from cart
    user can remove items from cart, cart does not show in navbar if it is empty, when item is removed from cart a flash message confirms and shows link back to item, cart index still needs styling

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -15,4 +15,22 @@ class CartController < ApplicationController
       [Product.find(id.to_i), quantity]
     end
   end
+
+  def destroy
+    id = params[:id]
+    name = find_product(id).name
+    @cart.contents.reject! {|k| k == id}
+    flash[:alert] = "You have removed #{view_context.link_to name, product_path(id)} from your cart."
+    if @cart.contents.empty?
+      redirect_to root_path
+    else
+      redirect_to cart_index_path
+    end
+  end
+
+private
+
+  def find_product(id)
+    Product.find(id)
+  end
 end
