2c08f03f7eae722f61e748e2e40e5210cebbef7a
Adam Hundley <adamhundley@gmail.com>
2016-03-03 16:03:48 -0700

    Refactor and moving down the stack
    * create the cart products controller in order to make the car path restful for the create/update actions
    * change cart resources to singular resource
    * change cart index to show
    * made display_price not take an argument
    * the cart now knows its products
    * show page now has @cart.products
    * create cart_product PORO
      * implemented simple delegation
      * we can now call product.quantity, product.price etc.

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,11 +1,11 @@
 module CartHelper
-  def product_total(product)
-    "$#{(product.last * product.first.price) / 100}"
-  end
+  # def product_total(product)
+  #   "$#{(product.last * product.first.price) / 100}"
+  # end
 
   def cart_total(products)
-      products.map do |product|
-      product.first.price * product.last
+    products.map do |product|
+      product.price * product.quantity
     end.reduce(:+) / 100
   end
 end
