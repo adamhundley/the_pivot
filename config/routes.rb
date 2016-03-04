8b0da5ed6b9908b579e1f03c4b6fa54da0b34629
David Whitaker <damwhit@gmail.com>
2016-03-03 18:56:07 -0700

    admin is able to log in and see the admin dashboard
    admin can now log in and see the admin dashboard which contains their name.  will add more info soon

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
     get "/orders/:order_id/thanks", to: "orders#thanks", as: "thanks"
   end
 
+  namespace :admin do
+    get "/dashboard", to: "users#show"
+  end
   resources :orders, only: [:new, :create]
 
   get "/login", to: "sessions#new"
