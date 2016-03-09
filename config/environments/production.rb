bcd10d9b66b9fbf4a115ce323b84bfedcacf0292
David Whitaker <damwhit@gmail.com>
2016-03-09 14:12:34 -0700

    heroku app should have email
    using send grid for production email checking to see if it works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,4 +34,18 @@ Rails.application.configure do
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'www.littleowl.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: "587",
+    domain: "heroku.com",
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
+  }
+
 end
