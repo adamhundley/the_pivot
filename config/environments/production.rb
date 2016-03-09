3b553f0c4e3dc86337644b11ca76d735e139adcf
David Whitaker <damwhit@gmail.com>
2016-03-09 14:35:21 -0700

    changed sengrid settings heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,13 +35,14 @@ Rails.application.configure do
     }
   }
 
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'www.littleowl.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'http://www.littleowl.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: "587",
-    domain: "heroku.com",
+    domain: "littleowl.heroku.com",
     authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV['SENDGRID_USERNAME'],
