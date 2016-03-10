6ad386ca43652ef4f3902090cb62207415dca65a
Adam Hundley <adamhundley@gmail.com>
2016-03-09 18:53:53 -0700

    styling da email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,6 @@ class OrderMailer < ApplicationMailer
   def order_email(order)
     @order = order
     @url = "http://littleowl.herokuapp.com"
-    mail(to: @order.email, subject: 'test confirmation email')
+    mail(to: @order.email, subject: 'Alright, Alright Alright.  Your joe is on the way.')
   end
 end
