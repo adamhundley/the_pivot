4d8a4e00d29c486532a1abeb7f7c2206232091e2
Nathan Venn <natevenn@gmail.com>
2016-03-23 07:30:30 -0600

    change database name to crash at my pad

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,14 +26,6 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
-  storage: :s3,
-    s3_credentials: {
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
@@ -43,6 +35,6 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-  
+
 
 end
