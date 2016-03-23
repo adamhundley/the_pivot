2941dfc3aea8cb4d8e6b7c72202e9709b7466f12
Nathan Venn <natevenn@gmail.com>
2016-03-23 08:02:46 -0600

    add paperclip config with aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,14 @@ Rails.application.configure do
 
   config.assets.raise_runtime_errors = true
 
-
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
