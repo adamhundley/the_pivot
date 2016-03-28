5471cf762b9abc674c8fb86856054063aa40aac4
Alex <alexxnavarretee@gmail.com>
2016-03-27 23:02:09 -0600

    deleted unneccessary files and one method that was not being used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-  def current_permission
-    @current_permission ||= Permission.new(current_user)
-  end
 end
