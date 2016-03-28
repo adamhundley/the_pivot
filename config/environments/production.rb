0b90535680b2742a488dd6b11299f9f24bd9125b
Adam Hundley <adamhundley@gmail.com>
2016-03-28 13:27:56 -0600

    add aws to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,15 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
+  config.paperclip_defaults = {
+  storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
