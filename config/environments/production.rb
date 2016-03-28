a0477dc090bb0b7ccde54b7f15d45c3033e7f984
Adam Hundley <adamhundley@gmail.com>
2016-03-28 13:31:51 -0600

    fixing aws credientials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
     s3_credentials: {
-      bucket: ENV['AWS_BUCKET'],
+      bucket: ENV['AWS_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      aws_region: ENV['AWS_REGION'],
     }
   }
 
