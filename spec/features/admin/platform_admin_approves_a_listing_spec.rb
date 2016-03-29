0bdbfb2696c734cdbd6750742e63c13a099ab645
Alex <alexxnavarretee@gmail.com>
2016-03-29 14:45:34 -0600

    Properties Can Go From Active to Inactive
    Platform admin has the ability to make properties go from active to inactive and
    vice-versa. Admin also now went from "disabled properties" views to "inactive
    properties." There's also a lot of more robust tests to help insure that there
    won't be any missing gaps.

--- spec/features/admin/platform_admin_approves_a_listing_spec.rb
+++ spec/features/admin/platform_admin_approves_a_listing_spec.rb
@@ -9,20 +9,17 @@ RSpec.feature "platform admin approves a property listing" do
                                 password: "password",
                                 role: 1)
 
-    type     = create(:property_type)
-    amenity  = create(:amenity)
+    user     = create(:user)
     property = create(:property)
 
-
-    platform_admin.properties << property
-              type.properties << property
-           amenity.properties << property
+    property.update(user_id: user.id)
 
     allow_any_instance_of(ApplicationController).to(
     receive(:current_user).and_return(platform_admin))
 
     visit admin_dashboard_path
     expect(current_path).to eq('/admin/dashboard')
+
 
     click_on "pending properties"
 
@@ -38,15 +35,24 @@ RSpec.feature "platform admin approves a property listing" do
     expect(page).to have_content("status")
 
     expect(page).to have_content("john adams")
-    refute property.status?
 
-    within "#1-property" do
+    within "##{property.id}-property" do
       expect(page).to have_content(property.id)
       expect(page).to have_button("update property")
-      select "true", from: "property_status"
+      select "active", from: "property_status"
     end
 
+    expect(property.status).to eq("pending")
+
     click_on "update property"
-    expect(page).to have_content("Cheerio! Property 1 has been updated!")
+    property.reload
+
+    within(:css, "h1.active.admin-property-index-header") do
+      expect(page).to have_content("active properties")
+    end
+
+    expect(page).to have_content("Property ID: #{property.id} from Owner #{property.owner} has been updated.")
+
+    expect(property.status).to eq("active")
   end
 end
